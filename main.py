3bd734aad27b23701ccbfa11503ad39e46be7581
CPirone <120253739+CPirone@users.noreply.github.com>
2024-02-26 23:14:38 +0000

    Dinamização das informações de requisição

--- main.py
+++ main.py
@@ -1,18 +1,36 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
-    return render_template('index.html', title='Avaliação contínua: Aula 030', options=["Home", "Identificação", "Contexto de requisição"])
+  return render_template(
+      'index.html',
+      title='Avaliação contínua: Aula 030',
+      options=["Home", "Identificacao", "Contexto_requisicao"])
+
 
 @app.route('/identificacao')
 def identificacao():
-    return render_template('identificacao.html', title='Avaliação contínua: Aula 030', aluno='Cleber Pirone', prontuario='PT3008649', instituicao='IFSP')
+  return render_template('identificacao.html',
+                         title='Avaliação contínua: Aula 030',
+                         aluno='Cleber Pirone',
+                         prontuario='PT3008649',
+                         instituicao='IFSP')
+
 
 @app.route('/contexto_requisicao')
 def contexto_requisicao():
-    return render_template('contexto_requisicao.html', title='Avaliação contínua: Aula 030', navegador='Google Chrome (Versão 122.0.6261.57 (Versão oficial) 64 bits)', ip='99.9.9.99', host='cpirone.pythonanywhere.com')
+  navegador = request.headers.get('User-Agent')
+  ip = request.remote_addr
+  host = request.host
+  return render_template('contexto_requisicao.html',
+                         title='Avaliação contínua: Aula 030',
+                         navegador=navegador,
+                         ip=ip,
+                         host=host)
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+  app.run(debug=True)
